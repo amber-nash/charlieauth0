9ff53f320f5216c956d61950cac4e8fe5b645dd9
Amber Nash <ambernash@Ambers-MBP-2.hsd1.ca.comcast.net>
2021-11-07 13:04:00 -0800

    remove backslash

--- authorization/urls.py
+++ authorization/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('logout', views.logout),
     path('', include('django.contrib.auth.urls')),
     path('', include('social_django.urls')),
-    path('/api/v2/users/{id}', views.v2search)
+    path('api/v2/users/{id}', views.v2search)
 ]
 
