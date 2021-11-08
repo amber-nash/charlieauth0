b3b4273b5547c97bd07b8272f35e6e168cd7c160
Amber Nash <ambernash@Ambers-MBP-2.hsd1.ca.comcast.net>
2021-11-07 22:50:38 -0800

    stuff

--- authorization/urls.py
+++ authorization/urls.py
@@ -36,9 +36,6 @@ urlpatterns = [
     path('logout', views.logout),
     path('', include('django.contrib.auth.urls')),
     path('', include('social_django.urls')),
-    path('api/public', views.public),
-    path('api/private', views.private),
-    path('api/private-scoped', views.private_scoped),
     path('api/v2/users/{id}', views.v2search)
 ]
 
