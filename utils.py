c00cb427d68fa6c9c4dea4189a2357535fbebf93
Amber Nash <ambernash@Ambers-MBP-2.hsd1.ca.comcast.net>
2021-11-07 22:52:57 -0800

    stuff

--- utils.py
+++ utils.py
@@ -1,27 +1,27 @@
-# auth0authorization/utils.py
+# # auth0authorization/utils.py
 
-from django.contrib.auth import authenticate
-import json
-import jwt
-import requests
+# from django.contrib.auth import authenticate
+# import json
+# import jwt
+# import requests
 
-def jwt_get_username_from_payload_handler(payload):
-    username = payload.get('sub').replace('|', '.')
-    authenticate(remote_user=username)
-    return username
+# def jwt_get_username_from_payload_handler(payload):
+#     username = payload.get('sub').replace('|', '.')
+#     authenticate(remote_user=username)
+#     return username
 
-    # auth0authorization/utils.py
+#     # auth0authorization/utils.py
 
-def jwt_decode_token(token):
-    header = jwt.get_unverified_header(token)
-    jwks = requests.get('https://{}/.well-known/jwks.json'.format('dev-aykh4rgv.us.auth0.com')).json()
-    public_key = None
-    for jwk in jwks['keys']:
-        if jwk['kid'] == header['kid']:
-            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+# def jwt_decode_token(token):
+#     header = jwt.get_unverified_header(token)
+#     jwks = requests.get('https://{}/.well-known/jwks.json'.format('dev-aykh4rgv.us.auth0.com')).json()
+#     public_key = None
+#     for jwk in jwks['keys']:
+#         if jwk['kid'] == header['kid']:
+#             public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
 
-    if public_key is None:
-        raise Exception('Public key not found.')
+#     if public_key is None:
+#         raise Exception('Public key not found.')
 
-    issuer = 'https://{}/'.format('dev-aykh4rgv.us.auth0.com')
-    return jwt.decode(token, public_key, audience='YOUR_API_IDENTIFIER', issuer=issuer, algorithms=['RS256'])
+#     issuer = 'https://{}/'.format('dev-aykh4rgv.us.auth0.com')
+#     return jwt.decode(token, public_key, audience='YOUR_API_IDENTIFIER', issuer=issuer, algorithms=['RS256'])
