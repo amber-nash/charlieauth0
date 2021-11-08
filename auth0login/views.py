9e063c244072accc3f41b01939546a57698eaf53
Amber Nash <ambernash@ambers-mbp-2.attlocal.net>
2021-11-07 20:30:01 -0800

    stuff

--- auth0login/views.py
+++ auth0login/views.py
@@ -82,8 +82,8 @@ def requires_scope(required_scope):
     # auth0authorization/views.py
 
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import AllowAny
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
