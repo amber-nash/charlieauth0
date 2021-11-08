078b7209d02ba96ddc89c6163241bea300e48352
Amber Nash <ambernash@Ambers-MBP-2.hsd1.ca.comcast.net>
2021-11-07 22:46:18 -0800

    stuff

--- auth0login/views.py
+++ auth0login/views.py
@@ -81,23 +81,23 @@ def requires_scope(required_scope):
 
     # auth0authorization/views.py
 
-from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+# from django.http import JsonResponse
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import AllowAny
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def public(request):
-    return JsonResponse({'message': 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'})
+# @api_view(['GET'])
+# @permission_classes([AllowAny])
+# def public(request):
+#     return JsonResponse({'message': 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'})
 
 
-@api_view(['GET'])
-def private(request):
-    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated to see this.'})
+# @api_view(['GET'])
+# def private(request):
+#     return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated to see this.'})
 
-# auth0authorization/views.py
+# # auth0authorization/views.py
 
-@api_view(['GET'])
-@requires_scope('read:messages')
-def private_scoped(request):
-    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'})
+# @api_view(['GET'])
+# @requires_scope('read:messages')
+# def private_scoped(request):
+#     return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'})
